e1c77bbe1d8e4712f97ddd150e227f47565d30be
itsrusty <rusty3031@gmail.com>
2024-01-26 12:34:39 -0700

    refactor code

--- src/routers/fichaIdentificacion.router.ts
+++ src/routers/fichaIdentificacion.router.ts
@@ -1,28 +1,37 @@
-import { Request , Response, Router } from "express";
-import { fichaIdentificacion } from "../controllers/fichaIdentificacion.Controllers";
+import { Request, Response, Router } from "express";
+import { fichaIdentificacion } from "../controllers/fichaIdentificacion.controllers";
 
+const controllerFichaIdentificacion = new fichaIdentificacion();
+const routerFichaIdentificacion = Router();
+const path = "/fichasIdentificacion";
 
-const controllerFichaIdentificacion= new fichaIdentificacion()
-const routerFichaIdentificacion=  Router();
+// todo: get all patients
+routerFichaIdentificacion.get(`${path}/all`, (req: Request, res: Response) => {
+  controllerFichaIdentificacion.getPatients(req, res);
+});
 
+// todo: get by username
+routerFichaIdentificacion.get(
+  `${path}/filterName`,
+  (req: Request, res: Response) => {
+    controllerFichaIdentificacion.getPatientsByName(req, res);
+  }
+);
 
-routerFichaIdentificacion.get('/fichasIdentificacion',(req:Request,res:Response)=>{
-    controllerFichaIdentificacion.getPatients(req,res);
-})
+// todo: show by filter
+routerFichaIdentificacion.get(
+  `${path}/filtered/:filter`,
+  (req: Request, res: Response) => {
+    controllerFichaIdentificacion.getPatientsFiltred(req, res);
+  }
+);
 
-
-routerFichaIdentificacion.get('/fichasIdentificacion/filtedName',(req:Request,res:Response)=>{
-    controllerFichaIdentificacion.getPatientsByName(req,res);
-})
-
-
-routerFichaIdentificacion.get('/fichasIdentificacion/filtred/:filter',(req:Request,res:Response)=>{
-    controllerFichaIdentificacion.getPatientsFiltred(req,res);
-})
-
-routerFichaIdentificacion.post('/fichaIdentificacion/new',(req:Request, res:Response)=>{
-    controllerFichaIdentificacion.createFichaIdentificacion(req,res)
-})
-
+// todo: create a new patient
+routerFichaIdentificacion.post(
+  `${path}/create-patient`,
+  (req: Request, res: Response) => {
+    controllerFichaIdentificacion.createFichaIdentificacion(req, res);
+  }
+);
 
 export default routerFichaIdentificacion;
